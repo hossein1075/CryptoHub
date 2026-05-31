b6a23c0607b4da28f4007cc989bd2dec7758d1f8
hossein hosseini <hosseinh1075@gmail.com>
2026-05-31 11:50:37 +0330

    add html navbar mobile

--- src/components/modules/navBar/NavMobile.tsx
+++ src/components/modules/navBar/NavMobile.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 function NavMobile() {
   return (
-    <div>NavMobile</div>
+     <nav>
+        <div>
+            <div>
+                <Image src='' alt= 'logo' width={44} height={44}/>
+                <div>
+                    <h2>BlueTrade</h2>
+                    <p>مدیریت مالی، بازار و خدمات دیجیتال</p>
+                </div>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    </nav>
   )
 }
 
