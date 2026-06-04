a6544f733b1a99b6de4685e1f54312d2840a5fb8
hossein hosseini <hosseinh1075@gmail.com>
2026-06-04 12:05:19 +0330

    add html crypto price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,54 @@
 import NavBar from "@/components/modules/navBar/NavBar";
 import SideBar from "@/components/modules/sidebar/SideBar";
-import Image from "next/image";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 
 export default function Home() {
   return (
   <>
+  <div>
+    <div>
+      {/* crypto */}
+      <div>
+        <span></span>
+        <span>دلار پی پال</span>
+        <span>۵۹,۴۰۰ تومان</span>
+        <span>
+          +۱.۲٪
+          <IoIosArrowUp/>
+        </span>
+      </div>
+      <div>
+        <span></span>
+        <span>دلار پی پال</span>
+        <span>۵۹,۴۰۰ تومان</span>
+        <span>
+          +۱.۲٪
+          <IoIosArrowUp/>
+        </span>
+      </div>
+      <div>
+        <span></span>
+        <span>دلار پی پال</span>
+        <span>۵۹,۴۰۰ تومان</span>
+        <span>
+          +۱.۲٪
+          <IoIosArrowDown/>
+        </span>
+      </div>
+      <div>
+        <span></span>
+        <span>دلار پی پال</span>
+        <span>۵۹,۴۰۰ تومان</span>
+        <span>
+          +۱.۲٪
+          <IoIosArrowDown/>
+        </span>
+      </div>
+    </div>
+  </div>
   <NavBar/>
-  <SideBar/>
+  {/* <SideBar/> */}
   </>
   );
 }
--- src/components/modules/sidebar/SideBar.tsx
+++ src/components/modules/sidebar/SideBar.tsx
@@ -11,8 +11,8 @@ function SideBar() {
                           <div className='flex items-center gap-3'>
                               <div className='size-11 flexCenter bg-linear-to-br from-blue-600 to-cyan-400 text-white rounded-2xl shadow-lg shadow-blue-500/30'>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="M12 3l7 4v10l-7 4-7-4V7l7-4z"></path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="M12 7l4 2.3v5.4L12 17l-4-2.3V9.3L12 7z"></path>
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.8" d="M12 3l7 4v10l-7 4-7-4V7l7-4z"></path>
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.8" d="M12 7l4 2.3v5.4L12 17l-4-2.3V9.3L12 7z"></path>
                                 </svg>
                             </div>
                             <div>
