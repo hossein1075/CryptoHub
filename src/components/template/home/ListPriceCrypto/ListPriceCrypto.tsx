a05ddd5921afec02687e71bfd4280fa382dabc66
hossein hosseini <hosseinh1075@gmail.com>
2026-06-12 12:07:29 +0330

    add btn section sixth pages home

--- src/components/template/home/ListPriceCrypto/ListPriceCrypto.tsx
+++ src/components/template/home/ListPriceCrypto/ListPriceCrypto.tsx
@@ -7,6 +7,7 @@ function ListPriceCrypto() {
         <>
             <section className='mt-15 font-lahze'>
                 <div className="container">
+                    {/* header section */}
                     <div className='flex flex-col md:flex-row justify-between items-center mb-8'>
                         <div className='text-right w-full md:w-auto mb-6 md:mb-0'>
                             <p className='text-sm mb-1 text-gray-500'>قیمت لحظه‌ای مهم‌ترین ارزها</p>
@@ -16,12 +17,12 @@ function ListPriceCrypto() {
                             <button className='flex items-center gap-2 font-bold text-gray-800'>
                                 <span className='font-bold'>
 
-                                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                                </svg>
+                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                                    </svg>
                                 </span>
                                 <span className='font-bold'>
-                                نمایش لیست
+                                    نمایش لیست
                                 </span>
                             </button>
                             <button className='flex items-center gap-2 text-gray-400 hover:text-gray-800 transition duration-150 ease-linear'>
@@ -34,7 +35,14 @@ function ListPriceCrypto() {
 
                         </div>
                     </div>
+                    {/* table */}
                     <CryptoTable />
+                    {/* button */}
+                    <div className='flex justify-center mt-8 mb-8'>
+                        <Button variant="white" size="sm" className="text-sm! shadow-sm rounded-full! font-normal! text-gray-600! border-2 border-gray-200!">
+                            مشاهده قیمت تمام ارزهای دیجیتال
+                        </Button>
+                    </div>
                 </div>
             </section>
         </>
