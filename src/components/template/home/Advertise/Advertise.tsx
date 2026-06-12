63cea76d71b043369d1a1d8158d66a61a1a9cd96
hossein hosseini <hosseinh1075@gmail.com>
2026-06-12 12:19:50 +0330

    add css section seventh pages home

--- src/components/template/home/Advertise/Advertise.tsx
+++ src/components/template/home/Advertise/Advertise.tsx
@@ -5,12 +5,12 @@ function Advertise() {
   return (
      <section className='mt-15 font-lahze'>
                     <div className="container">
-                        <div>
+                        <div className='flex flex-col md:flex-row items-center gap-10'>
                             <div>
-                                <Image width={590} height={140} src='/images/home/banner-exchange.avif' alt='AdvertisePic'/>
+                                <Image width={590} height={140} src='/images/home/banner-exchange.avif' className='hover:shadow-[0_0_20px_1px_#00000040] hover:scale-105 transition duration-500 ease-out rounded-20px' alt='AdvertisePic'/>
                             </div>
                             <div>
-                                <Image width={590} height={140} src='/images/home/banner-exchange.avif' alt='AdvertisePic'/>
+                                <Image width={590} height={140} src='/images/home/banner-exchange.avif' className='hover:shadow-[0_0_20px_1px_#00000040] hover:scale-105 transition duration-500 ease-out rounded-20px' alt='AdvertisePic'/>
                             </div>
                         </div>
                     </div>
