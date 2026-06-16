50506de6d31dadd2b777bca7042346f4f0d4892c
hossein hosseini <hosseinh1075@gmail.com>
2026-06-16 15:47:59 +0330

    add html ValuesWeb about us page

--- src/components/template/about-us/ValuesWeb/ValuesWeb.tsx
+++ src/components/template/about-us/ValuesWeb/ValuesWeb.tsx
@@ -1,9 +1,22 @@
+import TitleSec from '@/components/modules/TitleSec/TitleSec'
+import ValuesWebBox from '@/components/modules/ValuesWebBox/ValuesWebBox'
 import React from 'react'
 
 function ValuesWeb() {
     return (
         <section className='mt-15 font-lahze'>
             <div className="container">
+                <div>
+                    <TitleSec />
+                    <h2>اصولی که مسیر ما را مشخص می‌کند</h2>
+                    <p>تصمیم‌ها، توسعه محصول و نحوه ارتباط ما با کاربران بر پایه چند اصل کلیدی شکل گرفته است.</p>
+                </div>
+                <div>
+                    <ValuesWebBox/>
+                    <ValuesWebBox/>
+                    <ValuesWebBox/>
+                    <ValuesWebBox/>
+                </div>
 
             </div>
         </section>
