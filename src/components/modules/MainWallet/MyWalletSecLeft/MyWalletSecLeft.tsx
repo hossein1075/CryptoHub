2a0a1167b6b3d53a544f645ab2bcbb321d0e780d
hossein hosseini <hosseinh1075@gmail.com>
2026-06-24 19:05:03 +0330

    add css div left of first section wallet page

--- src/components/modules/MainWallet/MyWalletSecLeft/MyWalletSecLeft.tsx
+++ src/components/modules/MainWallet/MyWalletSecLeft/MyWalletSecLeft.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import MyWalletSecLeftBox from '../MyWalletSecLeftBox/MyWalletSecLeftBox'
 
 function MyWalletSecLeft() {
   return (
-    <div>
-        <h3>خلاصه سریع</h3>
-        <div>
+    <div className='backdrop-blur-xl border border-indigo-50 bg-white/90 p-6 rounded-34px shadow-[0_24px_60px_rgba(15,23,42,0.06)]'>
+        <h3 className='text-lg text-gray-900 mb-5 font-black'>خلاصه سریع</h3>
+        <div className='space-y-4'>
             {/* # */}
+            <MyWalletSecLeftBox/>
+            <MyWalletSecLeftBox/>
+            <MyWalletSecLeftBox/>
         </div>
     </div>
   )
--- src/components/modules/MainWallet/MyWalletSecLeftBox/MyWalletSecLeftBox.tsx
+++ src/components/modules/MainWallet/MyWalletSecLeftBox/MyWalletSecLeftBox.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 function MyWalletSecLeftBox() {
-    return (
-            <div>بیشترین دارایی</div>
+  return (
+    <div className='rounded-24px bg-slate-50 border border-indigo-50 p-4'>
+      <div className='text-xs font-bold text-gray-500'>بیشترین دارایی</div>
+    </div>
   )
 }
 
