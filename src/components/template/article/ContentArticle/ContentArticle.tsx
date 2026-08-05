b645e956909b40057ebafc9db7e5c15a12b0e5d3
hossein hosseini <hosseinh1075@gmail.com>
2026-08-05 11:57:20 +0330

    add article section part 1 in article page

--- src/components/template/article/ContentArticle/ContentArticle.tsx
+++ src/components/template/article/ContentArticle/ContentArticle.tsx
@@ -1,8 +1,17 @@
+import ContentArticleContent from '@/components/modules/MainArticle/ContentArticleContent/ContentArticleContent'
 import React from 'react'
+import ContentArticleAside from '../ContentArticleAside/ContentArticleAside'
 
 function ContentArticle() {
   return (
-    <div>ContentArticle</div>
+    <section className='font-lahze mt-15'>
+      <div className="container">
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-8'>
+          <ContentArticleContent/>
+          <ContentArticleAside/>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/template/article/ContentArticleAside/ContentArticleAside.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContentArticleAside() {
+  return (
+    <aside></aside>
+  )
+}
+
+export default ContentArticleAside
